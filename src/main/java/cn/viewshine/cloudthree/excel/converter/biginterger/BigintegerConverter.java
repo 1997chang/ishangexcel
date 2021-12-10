1ddc69719d98a12fd3cb5282c4e494950a390c2f
changxiao <546613470@qq.com>
2021-12-10 18:38:16 +0800

    添加Biginteger转化器以及转化异常

---
+++ src/main/java/cn/viewshine/cloudthree/excel/converter/biginterger/BigintegerConverter.java
@@ -0,0 +1,16 @@
+package cn.viewshine.cloudthree.excel.converter.biginterger;
+
+import cn.viewshine.cloudthree.excel.converter.Converter;
+
+import java.math.BigInteger;
+
+/**
+ * @author ChangWei[changwei@viewshine.cn]
+ */
+public class BigintegerConverter implements Converter<BigInteger> {
+
+    @Override
+    public String converter(BigInteger bigInteger) {
+        return bigInteger.toString();
+    }
+}
